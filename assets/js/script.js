c59bf42216bcee47a44d5fc8d1ec36a8b08813b0
kashelton17 <81379573+kashelton17@users.noreply.github.com>
2021-05-13 09:19:17 -0400

    Adding console.log to see if problem is still happening

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,7 @@ for (var j=0;j<dayCard.length;j++) {
 // getting the local storage for the search history
 var searchHistory = [localStorage.getItem('history')]
 if (searchHistory.length > 0) {
+    console.log('search History is not null')
     searchHistory = searchHistory[0].split(',')
     for (var i=0; i < 10; i++){
         var listItem = document.createElement('li')
