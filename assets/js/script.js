50edbd8a8e81d4d260ff007b32bdb470c8739352
kashelton17 <81379573+kashelton17@users.noreply.github.com>
2021-05-13 09:15:08 -0400

    Fixing search History issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ for (var j=0;j<dayCard.length;j++) {
 
 // getting the local storage for the search history
 var searchHistory = [localStorage.getItem('history')]
-if (searchHistory !== null) {
+if (searchHistory.length > 0) {
     searchHistory = searchHistory[0].split(',')
     for (var i=0; i < 10; i++){
         var listItem = document.createElement('li')
